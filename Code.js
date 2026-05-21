5119c75814e0147d79123baadd5614f20c253b4d
Ludrein Reimar Salvador <ludreinreimar.salvador@gmail.com>
2026-05-21 12:59:32 +0800

    fix: SIA app name + tempo logo

--- Code.js
+++ Code.js
@@ -1,5 +1,17 @@
 function doGet() {
-  return HtmlService
-    .createHtmlOutputFromFile('Index')
-    .setTitle('COMMSCHED Chat App');
+  const template = HtmlService.createTemplateFromFile('Index');
+  template.logoDataUrl = getLogoDataUrl_();
+
+  return template
+    .evaluate()
+    .setTitle('SIA Chat App');
+}
+
+function getLogoDataUrl_() {
+  const fileId = '10AlyHkfuFJm3yUDhK_Oorxb3B_SgYVUb';
+  const blob = DriveApp.getFileById(fileId).getBlob();
+  const contentType = blob.getContentType() || 'image/png';
+  const base64 = Utilities.base64Encode(blob.getBytes());
+
+  return 'data:' + contentType + ';base64,' + base64;
 }
